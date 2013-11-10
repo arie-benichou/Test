897efbbcefb463d23f1b62971520bb1bc038dbd1
arie.benichou <arie.benichou@gmail.com>
2013-11-10 20:58:16 +0100

    small refactoring

--- src/main/scala/exercice1/StringCalculator.scala
+++ src/main/scala/exercice1/StringCalculator.scala
@@ -2,24 +2,27 @@ package exercice1
 
 object StringCalculator {
 
-  val pattern = "(?s)^//(.+?)\n(.*)".r
+  // You can follow a string with .r, turning it into a Regex.
+  // (?s): flag for multi-line mode
+  // +?  : non-greedy version of +
+  val pattern = "(?s)^//([^0-9\n]+?)\n(.*)".r
 
-  private def parse(input: String) = {
-    input match {
-      case pattern(head, tail) => (head, tail)
-      case _                   => (",", input)
-    }
+  // You can use pattern matching with regexp captures
+  private def parse(input: String) = input match {
+    case pattern(head, tail) => (head, tail)
+    case _                   => (",", input)
   }
 
-  private def parseInt(input: String): Int = if (input.isEmpty) 0 else Integer.parseInt(input)
+  private def parseInt(input: String) = if (input.isEmpty) 0 else Integer.parseInt(input)
 
-  def add(input: String): Int = {
+  // StringCalculator.apply(x) <=> StringCalculator(x)
+  def apply(input: String): Int = {
     val (head, tail) = parse(input)
     val parts = tail.split(head + "|\n")
     val integers = parts.map(parseInt)
     val negatives = integers.filter(_ < 0)
     if (negatives.isEmpty) integers.filterNot(_ > 1000).sum
-    else error("negatives not allowed: " + negatives.mkString(","))
+    else error("negatives not allowed: " + negatives.mkString(" "))
   }
 
 }
--- src/test/scala/exercice1/StringCalculatorTest.scala
+++ src/test/scala/exercice1/StringCalculatorTest.scala
@@ -10,55 +10,56 @@ class StringCalculatorTest extends FunSpec {
   describe("[StringCalculator]") {
 
     it("should return 0 for an empty string") {
-      assert(StringCalculator.add("") === 0)
+      assert(StringCalculator("") === 0) // => assertEquals(StringCalculator(""), 0)
     }
 
     it("should return 1 for \"1\"") {
-      assert(StringCalculator.add("1") === 1)
+      assert(StringCalculator("1") === 1)
     }
 
     it("should return 2 for \"2\"") {
-      assert(StringCalculator.add("2") === 2)
+      assert(StringCalculator("2") === 2)
     }
 
     it("should return 3 for \"1,2\"") {
-      assert(StringCalculator.add("1,2") === 3)
+      assert(StringCalculator("1,2") === 3)
     }
 
     it("should return 6 for \"1,2,3\"") {
-      assert(StringCalculator.add("1,2,3") === 6)
+      assert(StringCalculator("1,2,3") === 6)
     }
 
     it("should handle an unknown amount of numbers") {
-      assert(StringCalculator.add("1,2,3,4") === 10)
+      assert(StringCalculator("1,2,3,4") === 10)
     }
 
     it("should handle new lines as delimiter") {
-      assert(StringCalculator.add("1\n2,3") === StringCalculator.add("1,2,3"))
+      assert(StringCalculator("1\n2,3") === StringCalculator("1,2,3"))
     }
 
     it("should handle option for a different delimiter") {
-      assert(StringCalculator.add("//;\n1;2") === StringCalculator.add("1,2"))
-      assert(StringCalculator.add("//;\n5;2\n100") === StringCalculator.add("5,2,100"))
+      assert(StringCalculator("//,\n" + "1,2,3") === StringCalculator("1,2,3"))
+      assert(StringCalculator("//:\n" + "1:2:3") === StringCalculator("1,2,3"))
+      assert(StringCalculator("//:\n" + "1:2\n3") === StringCalculator("1,2,3"))
     }
 
     it("should not allow negative numbers") {
       try {
-        StringCalculator.add("1,-2,3,-4")
+        StringCalculator("1,-2,3,-4")
       }
       catch {
         case e: Exception => {
-          assert(e.getMessage === "negatives not allowed: -2,-4")
+          assert(e.getMessage === "negatives not allowed: -2 -4")
         }
       }
     }
 
     it("should ignore numbers greater than 1000") {
-      assert(StringCalculator.add("1,1000") === 1001)
-      assert(StringCalculator.add("1,1001") === 1)
-      assert(StringCalculator.add("2,1001") === 2)
-      assert(StringCalculator.add("2,1001,1002") === 2)
-      assert(StringCalculator.add("1001") === 0)
+      assert(StringCalculator("1,1000") === 1001)
+      assert(StringCalculator("1,1001") === 1)
+      assert(StringCalculator("2,1001") === 2)
+      assert(StringCalculator("2,1001,1002") === 2)
+      assert(StringCalculator("1001") === 0)
     }
 
   }
